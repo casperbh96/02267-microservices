14f5edbb7e4be62ced5dfedc96b585896e3bd607
Dmitry <rachkovskydmitry@gmail.com>
2020-01-13 14:52:50 +0100

    Changed cucumber test

--- src/test/java/com/dtupay/cucumber/utils/BankFactory.java
+++ src/test/java/com/dtupay/cucumber/utils/BankFactory.java
@@ -1,12 +1,13 @@
 package com.dtupay.cucumber.utils;
 
+import com.dtupay.bank.BankAdapterJar;
+import com.dtupay.bank.IBankAdapter;
 import dtu.ws.fastmoney.Bank;
 import dtu.ws.fastmoney.persistency.InMemoryRepository;
 
 public class BankFactory {
-	public Bank createBank() {
-		Bank bank = new Bank();
-		bank.setRepository(new InMemoryRepository());
+	public IBankAdapter createBank() {
+		IBankAdapter bank = new BankAdapterJar();
 		return bank;
 	}
 }
