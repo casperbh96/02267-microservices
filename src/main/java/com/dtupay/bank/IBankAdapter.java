7eadebcfda21eae98ac0662ccad51c965f7cd049
Dmitry <rachkovskydmitry@gmail.com>
2020-01-13 13:49:42 +0100

    Changes to bank adapter
    Use CPR as id for both clients and merchants
    Use BigDecimal for amounts of money

--- src/main/java/com/dtupay/bank/IBankAdapter.java
+++ src/main/java/com/dtupay/bank/IBankAdapter.java
@@ -4,10 +4,12 @@ import com.dtupay.app.Customer;
 import com.dtupay.app.Merchant;
 import dtu.ws.fastmoney.BankServiceException;
 
+import java.math.BigDecimal;
+
 public interface IBankAdapter {
-    void createAccount(Customer customer, int initialBalance);
+    void createAccount(Customer customer, BigDecimal initialBalance) throws BankServiceException;
 
-    void createAccount(Merchant merchant, int initialBalance);
+    void createAccount(Merchant merchant, BigDecimal initialBalance) throws BankServiceException;
 
-    void makeTransaction(Customer from, Merchant to, int amount, String comment) throws BankServiceException;
+    void makeTransaction(Customer from, Merchant to, BigDecimal amount, String comment) throws BankServiceException;
 }
