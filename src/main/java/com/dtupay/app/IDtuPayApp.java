cf6dac4f462b683153cbbfae40b33c65d33eef7f
Dmitry <rachkovskydmitry@gmail.com>
2020-01-13 15:13:06 +0100

    Do not catch BankServiceException

--- src/main/java/com/dtupay/app/IDtuPayApp.java
+++ src/main/java/com/dtupay/app/IDtuPayApp.java
@@ -1,10 +1,11 @@
 package com.dtupay.app;
 
 import com.dtupay.app.Token;
+import dtu.ws.fastmoney.BankServiceException;
 
 import java.math.BigDecimal;
 
 public interface IDtuPayApp {
     boolean checkTokenValidity(Token token);
-    void transferMoney(String merchantId, Token merchantToken, BigDecimal amount, String description);
+    void transferMoney(String merchantId, Token merchantToken, BigDecimal amount, String description) throws BankServiceException;
 }
