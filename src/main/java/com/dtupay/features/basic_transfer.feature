0b5ec2d3658f475a651bba8fd38f898be42b2faf
anshch22 <chauhananshul385@gmail.com>
2020-01-16 00:02:39 +0100

    merge cucumber test

--- src/main/java/com/dtupay/features/basic_transfer.feature
+++ src/main/java/com/dtupay/features/basic_transfer.feature
@@ -8,3 +8,11 @@ Feature: Basic DTU Pay money transfer
     Then the amount 300 is transferred to the merchant
     And the balance of the customer is 700
     And the balance of the merchant is 300
+
+  Scenario: Unsuccessful transfer
+    Given The bank account "Bob Clark", with ID "220866-2859" with starting balance 1000
+    And The bank account "Alice Klaus", with ID "220866-2858" with starting balance 0
+    And The customer DTU Pay account "Bob Clark", with ID "220866-2859", and 1 invalid token
+    And The merchant DTU Pay account "Alice Klaus", with ID "220866-2858"
+    When The merchant scans the customer's invalid token
+    Then Token is not found in the DTUPay system
--- src/test/java/com/dtupay/cucumber/steps/BasicTransferSteps.java
+++ src/test/java/com/dtupay/cucumber/steps/BasicTransferSteps.java
@@ -12,6 +12,8 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -30,6 +32,9 @@ public class BasicTransferSteps {
 
     Token token;
     boolean scanSuccessful;
+
+    Customer customer;
+    Merchant merchant;
 
 
     public BasicTransferSteps(Helper helper) {
@@ -92,4 +97,40 @@ public class BasicTransferSteps {
     public void the_balance_of_the_merchant_is(int arg1) throws Throwable {
         assertEquals(new BigDecimal(arg1), bank.getBalanceByCPR(merchantId));
     }
+
+    @Given("^The bank account \"([^\"]*)\", with ID \"([^\"]*)\" with starting balance (\\d+)$")
+    public void The_bank_account_with_starting_balance(String name, String cpr, int startingBalance) throws Throwable {
+        helper.createBankAccount(name, cpr, startingBalance);
+    }
+
+    @Given("^The customer DTU Pay account \"([^\"]*)\", with ID \"([^\"]*)\", and (\\d+) invalid token$")
+    public void ThecustomerDTUPayAccountIDAndInvalidToken(String name, String cpr, int numOfTokens) throws Throwable {
+        customerId = helper.createDtuPayCustomerUsedToken(name, cpr, numOfTokens).getId();
+
+    }
+
+    @Given("^The merchant DTU Pay account \"([^\"]*)\", with ID \"([^\"]*)\"")
+    public void ThemerchantDTUPayAccountIDAndInvalidToken(String name, String cpr) throws Throwable {
+        merchantId = helper.createDtuPayMerchant(name, cpr).getId();
+    }
+
+    @When("^The merchant scans the customer's invalid token$")
+    public void The_merchant_scans_the_customer_s_invalid_token() throws Throwable {
+        dtuPayApp = new DtuPayApp(bank, customers, merchants, tokens);
+
+        customer = customers.getCustomerByCustomerId(customerId);
+        merchant = merchants.getMerchantByMerchantId(merchantId);
+
+        customer.setDtuPay(dtuPayApp);
+        merchant.setDtuPay(dtuPayApp);
+    }
+
+    @Then("^Token is not found in the DTUPay system$")
+    public void TokenIsNotFoundInTheDTUPaySystem() {
+        // Write code here that turns the phrase above into concrete actions
+        Token token = new Token();
+        assertEquals(tokens.checkExists(token),false);
+    }
 }
+
+
