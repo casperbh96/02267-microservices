cdc1a2ec18792342f7a042f9a8ccbcb022d9c4fa
Dmitry <rachkovskydmitry@gmail.com>
2020-01-15 14:35:37 +0100

    added BankAdapterException

---
+++ src/main/java/com/dtupay/bank/exceptions/BankAdapterException.java
@@ -0,0 +1,7 @@
+package com.dtupay.bank.exceptions;
+
+public class BankAdapterException extends Exception {
+    public BankAdapterException(String errorMessage) {
+        super(errorMessage);
+    }
+}
