3b015560d44e8866a5291c1302cfe7b68cbc3aa1
Casper Bøgeskov Hansen <casperbh.96@gmail.com>
2020-01-17 15:08:47 +0100

    Create CustomerResourceTest.java

---
+++ src/test/java/com/dtupay/junit/CustomerResourceTest.java
@@ -0,0 +1,28 @@
+package com.dtupay.junit;
+
+import com.sun.net.httpserver.HttpServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
+public class CustomerResourceTest {
+
+    @Before
+    public void Setup(){
+
+    }
+
+    @After
+    public void cleanUp() {
+
+    }
+
+    @Test
+    public void getAllCustomersTest() {
+
+    }
+}
