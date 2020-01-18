6deef81b8e8d150134e938841ca6b76a9b1e15b2
Dmitry <rachkovskydmitry@gmail.com>
2020-01-18 17:45:59 +0100

    Added rese service for transaction manager

--- src/main/java/com/dtupay/app/ITransactionManager.java
+++ src/main/java/com/dtupay/app/ITransactionManager.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface ITransactionManager {
     List<Transaction> getCustomerMonthlyReport(int customerId);
+
     List<Transaction> getMerchantMonthlyReport(int merchantId);
+
     Transaction registerTransaction(Timestamp timestamp, int fromId, int toId, int tokenId, BigDecimal amount, boolean isRefund);
 }
--- src/main/java/com/dtupay/service/TransactionResource.java
+++ src/main/java/com/dtupay/service/TransactionResource.java
@@ -1,17 +1,68 @@
 package com.dtupay.service;
 
-import com.dtupay.BusinessLogic.BusinessLogicForTransaction;
-import com.dtupay.BusinessLogic.IBusinessLogicForTransaction;
+import com.dtupay.app.ITransactionManager;
+import com.dtupay.app.TransactionManager;
+import com.dtupay.database.TransactionAdapter;
+import org.json.JSONObject;
 
-import javax.ws.rs.Consumes;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+import java.math.BigDecimal;
+import java.sql.Timestamp;
+import java.util.UUID;
 
 @Path("/transaction")
 @Produces("application/json")
 @Consumes("application/json")
 public class TransactionResource {
-    private static IBusinessLogicForTransaction t = new BusinessLogicForTransaction();
+    ITransactionManager transactions = new TransactionManager(new TransactionAdapter());
 
-    
+    @GET
+    @Path("customer/{id}")
+    public Response getMonthlyReportForCustomer(@PathParam("id") int id) {
+        if (id == 0) {
+            return Response.status(400).entity("Missing parameters.").build();
+        }
+        try {
+            return Response.ok(transactions.getCustomerMonthlyReport(id)).build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+
+    @GET
+    @Path("merchant/{id}")
+    public Response getMonthlyReportForMerchant(@PathParam("id") int id) {
+        if (id == 0) {
+            return Response.status(400).entity("Missing parameters.").build();
+        }
+        try {
+            return Response.ok(transactions.getMerchantMonthlyReport(id)).build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+
+    @POST
+    @Consumes("application/json")
+    public Response postNewToken(String jsonRaw) {
+        JSONObject json = new JSONObject(jsonRaw);
+
+        Timestamp timestamp = Timestamp.valueOf(json.getString("timestamp"));
+        int fromId = json.getInt("fromId");
+        int toId = json.getInt("toId");
+        int tokenId = json.getInt("tokenId");
+        BigDecimal amount = json.getBigDecimal("amount");
+        boolean isRefund = json.getBoolean("isRefund");
+
+        if ((fromId == 0) || (toId == 0) || (tokenId == 0)) {
+            return Response.status(400).entity("Missing parameters.").build();
+        }
+        try {
+            return Response.ok(transactions.registerTransaction(timestamp, fromId, toId, tokenId, amount, isRefund)).build();
+        } catch (Exception e) {
+            return Response.serverError().build();
+        }
+    }
+
 }
